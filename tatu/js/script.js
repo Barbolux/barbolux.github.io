76e705a9935a9d74da492d3c761b5113927aebc0
Barbolux <tefoz@gmx.com>
2018-06-18 12:27:39 -0300

    Batch update

--- tatu/js/script.js
+++ tatu/js/script.js
@@ -6,4 +6,4 @@
   js = d.createElement(s); js.id = id;
   js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.0';
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', '
